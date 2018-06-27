d46a49cdbf060ecf71c4568c9788dda3fbb83e4d
BEAZERS BUSINESS LLC <wilfred.smithandsmith@gmail.com>
2018-06-26 20:26:31 -0400

    Button

--- src/pages/button.js
+++ src/pages/button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button, View, Text } from 'react-native';
 
 
-export default class DetailsScreen extends React.Component {
+export default class button extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
