5ce7fb8eea1c950e6dfec1029ec1ed3b1a0a8788
BEAZERS BUSINESS LLC <wilfred.smithandsmith@gmail.com>
2018-06-26 20:04:13 -0400

    yes

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ class HomeScreen extends React.Component {
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen</Text>
         <Button
-          title="Go to Details "
+          title="Go to Details sir "
           onPress={() => this.props.navigation.navigate('Details')}
         />
       </View>
